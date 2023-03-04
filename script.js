784d496de8aadbd20691ea52818135cab87d3c26
Helena Heil <helenahcodes@gmail.com>
2023-03-04 15:27:07 +0100

    add slide in on scroll

--- script.js
+++ script.js
@@ -385,3 +385,35 @@ function createCertificates() {
 }
 
 createCertificates();
+
+// Slide in on Scroll
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const slides = document.querySelectorAll(".slide-in");
+
+function checkSlide() {
+  slides.forEach((slide, i) => {
+    const slideTop = slide.getBoundingClientRect().top;
+    const isInWindow =
+      slideTop <=
+      (window.innerHeight || document.documentElement.clientHeight) / 1.25;
+    isInWindow && slide.classList.add("active");
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
